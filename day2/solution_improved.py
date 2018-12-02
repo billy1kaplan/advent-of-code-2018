f7d78a6c401c5b6b8b9690452998d5e3520bd0d2
Billy Kaplan <billy1kaplan@gmail.com>
2018-12-02 01:06:56 -0500

    Day 2 Improvement

--- day2/solution_improved.py
+++ day2/solution_improved.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 
-    #Part 1
+    # Part 1
     # defaultdict<char, int> -> boolean
     def count(frequencies, n):
         return len(list(filter(lambda x: x == n, counts.values()))) > 0
@@ -19,16 +19,15 @@ with open('input.txt', 'r') as f:
         if count(counts, 3): count3 += 1 
     print(count2 * count3)
 
-    def difference(word1, word2):
-        return len(list(filter(lambda char_tup: not char_tup[0] == char_tup[1], zip(word1, word2))))
-
-    def make_same(word1, word2):
+    # Part 2
+    def common(word1, word2):
         return ''.join([ch1 for ch1, ch2 in zip(word1, word2) if ch1 == ch2])
 
     def find_one_off():
         for line in lines:
             for other_line in lines:
-                if difference(line, other_line) == 1:
-                    print(make_same(line, other_line))
+                common_chars = common(line, other_line)
+                if len(common_chars) == len(line) - 1:
+                    print(common_chars)
                     return None
     find_one_off()
